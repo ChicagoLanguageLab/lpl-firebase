31ee88f2ce4cd69fb8bccface52b2e83b2e14ff3
Syuurinn <allisonkramer@uchicago.edu>
2017-06-01 10:05:37 -0500

    Change all 'choices' keys to 'cont_key' in 'text' modules; add browser test; add pseudorandom value to save file name and change location

--- public/static/js/production/experiment.js
+++ public/static/js/production/experiment.js
@@ -20,7 +20,25 @@ if(workerId == undefined || workerId == "")
     workerId = "TEST";
 
 var code = 'TURK' + jsPsych.randomization.randomID(10);
-var dataRef = storageRef.child('production/4-15-2017/' + workerId + '.csv');
+var salt = Math.floor(Math.random() * 100);
+
+var dataRef = storageRef.child('production-OSPAN/06-01-2017/' + params.workerId + '_' + salt + '.csv');
+
+browser_test_block = {
+  timeline: [{
+      type: "text",
+      cont_key: ['Y', 'N'],
+      text: '<p>This experiment works best if you have maximized your browser window. Is your browser window maximized?</p><p>Press <b>Y</b> for "yes" and <b>N</b> for "no".</p>',
+      timing_post_trial: 1000
+  }],
+  loop_function: function(data){
+    if(jsPsych.pluginAPI.convertKeyCharacterToKeyCode('n') == data[0].key_press){
+        return true;
+    } else {
+        return false;
+    }
+  }
+}
 
 /* Init and add production trials */
 
@@ -30,6 +48,7 @@ var productionInstructionsblock = initInstructions(productionInstructions, [' ']
 var productionBlock = initProductionTrials();
 
 timeline.push(productionInstructionsblock);
+timeline.push(browser_test_block);
 timeline = addObjectsToTimeline(timeline, productionBlock);
 
 var productionEndBlock = initInstructions(productionEndInstructions, [' ']);
@@ -59,8 +78,8 @@ timeline = addObjectsToTimeline(timeline, experimentBlock);
 
 var feedbackBlock = {
   type: "text",
-  text: function() { return "<p>You have finished the memory task. In case you are interested, these are your final results:</p><p>You answered " + (results.total_math_problems - results.total_math_wrong) + " math problems correctly out of " + results.total_math_problems + " total problems. Of your incorrect answers, " + results.total_math_accuracy_errors + " were accuracy errors, and " + results.total_math_speed_errors + " were speed errors.</p><p>You recalled " + results.total_letters_correct + " letters correctly out of " + results.total_letters + " total letters. You responded with 100% accuracy on " + results.total_strings_correct + " strings out of " + results.total_strings + " total strings.</p><p>Press <strong>space</strong> to continue to your survey code.</p>"; },
-  choices: [' '],
+  text: function() { return "<p>You have finished the memory task. These are your final results:</p><p>You answered " + (results.total_math_problems - results.total_math_wrong) + " math problems correctly out of " + results.total_math_problems + " total problems. Of your incorrect answers, " + results.total_math_accuracy_errors + " were accuracy errors, and " + results.total_math_speed_errors + " were speed errors.</p><p>You recalled " + results.total_letters_correct + " letters correctly out of " + results.total_letters + " total letters. You responded with 100% accuracy on " + results.total_strings_correct + " strings out of " + results.total_strings + " total strings.</p><p>Press <strong>space</strong> to continue to your survey code.</p>"; },
+  cont_key: [' '],
   on_finish: function() {
     addWorker(workerId, "production-OSPAN");
     saveData(jsPsych.data.dataAsCSV(), dataRef);
@@ -71,7 +90,7 @@ timeline.push(feedbackBlock);
 
 var endBlock = {
   type: "text",
-  choices: [''],
+  cont_key: [''],
   text: function(){
       return "<p>Thank you for your participation! Your responses have been saved.</p><p>Your survey code is <b>" + code + "</b>. Please enter this code into your HIT. You may then close this window.</p><p>If you have any questions or concerns, please do not hesitate to contact the lab at <a href='mailto:uchicagolanglab@gmail.com'>uchicagolanglab@gmail.com</a>.";
   }
