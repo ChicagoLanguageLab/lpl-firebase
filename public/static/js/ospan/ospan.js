1195e6d84e9301521fb0244e12276bafa9a529b5
Allison Kramer <a.kramer103@gmail.com>
2017-07-25 10:55:01 -0500

    Add survey code, change save location, add worker check

--- public/static/js/ospan/ospan.js
+++ public/static/js/ospan/ospan.js
@@ -16,11 +16,12 @@ var database = firebase.database();
 var params = getAllUrlParams();
 
 var workerId = params.workerId;
-if(workerId == undefined || workerId == "")
+if(workerId == undefined || workerId == "") {
     workerId = "TEST";
+}
 
 var code = 'TURK' + jsPsych.randomization.randomID(10);
-var dataRef = storageRef.child('OSPAN-only/07-18-2017/' + workerId + '.csv');
+var dataRef = storageRef.child('OSPAN-only/07-25-2017/' + workerId + '.csv');
 
 /* Generate OSPAN sections */
 
@@ -34,11 +35,6 @@ var bothPracticeBlock = makeOspanTrials(pracBothSetSize, "BothPractice");
 var experimentInstructionsBlock = initInstructions(experimentInstructions, [' ']);
 var experimentBlock = makeOspanTrials(jsPsych.randomization.shuffle(testBothSetSize), "Experiment");
 
-var feedbackBlock = {
-  type: "text",
-  text: function() { return "<p>You have finished the memory task. These are your final results:</p><p>You answered " + (results.total_math_problems - results.total_math_wrong) + " math problems correctly out of " + results.total_math_problems + " total problems. Of your incorrect answers, " + results.total_math_accuracy_errors + " were accuracy errors, and " + results.total_math_speed_errors + " were speed errors.</p><p>You recalled " + results.total_letters_correct + " letters correctly out of " + results.total_letters + " total letters. You responded with 100% accuracy on " + results.total_strings_correct + " strings out of " + results.total_strings + " total strings.</p><p>Thank you for your participation!</p>"; }
-};
-
 /* Add sections to timeline */
 
 var timeline = [];
@@ -51,15 +47,37 @@ timeline.push(bothPracticeInstructions);
 timeline = addObjectsToTimeline(timeline, bothPracticeBlock);
 timeline.push(experimentInstructionsBlock);
 timeline = addObjectsToTimeline(timeline, experimentBlock);
+
+var feedbackBlock = {
+  type: "text",
+  text: function() { return "<p>You have finished the memory task. These are your final results:</p><p>You answered " + (results.total_math_problems - results.total_math_wrong) + " math problems correctly out of " + results.total_math_problems + " total problems. Of your incorrect answers, " + results.total_math_accuracy_errors + " were accuracy errors, and " + results.total_math_speed_errors + " were speed errors.</p><p>You recalled " + results.total_letters_correct + " letters correctly out of " + results.total_letters + " total letters. You responded with 100% accuracy on " + results.total_strings_correct + " strings out of " + results.total_strings + " total strings.</p><p>Press <strong>space</strong> to continue to your survey code.</p>"; },
+  cont_key: [' '],
+  on_finish: function() {
+    addWorker(workerId, "production-study");
+    saveData(jsPsych.data.dataAsCSV(), dataRef);
+  }
+};
+
 timeline.push(feedbackBlock);
+
+var endBlock = {
+  type: "text",
+  cont_key: [''],
+  text: function(){
+      return "<p>Thank you for your participation! Your responses have been saved.</p><p>Your survey code is <b>" + code + "</b>. Please enter this code into your HIT. You may then close this window.</p><p>If you have any questions or concerns, please do not hesitate to contact the lab at <a href='mailto:uchicagolanglab@gmail.com'>uchicagolanglab@gmail.com</a>.";
+  }
+};
+
+timeline.push(endBlock);
 
 /* Start the experiment */
 
 $(document).ready(function(){
-    checkWorker(workerId, 'production-study');
 
     $('#progress-bar').show();
     $('#jspsych-target').show();
+
+    checkWorker(workerId, 'production-study', 'jspsych-target');
 
     jsPsych.init({
       display_element: $('#jspsych-target'),
