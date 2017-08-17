096b738da0b291882389fb84975e8d1296fb96e6
Narquelion <a.kramer103@gmail.com>
2017-08-16 20:06:44 -0500

    Organize data into objects

--- public/static/js/adaptation/data.js
+++ public/static/js/adaptation/data.js
@@ -1,68 +1,63 @@
 // data.js
-// Static data for the adaptation experiment
+// Data for the adaptation experiment
 
-/* Static jsPsych trials */
+/* Premade jsPsych trials */
 
-// Initial text shown to the subject
-var pre_consent = {
-  type: 'text',
-  text: `<div class="header row">
-           <div class="col-2 text-right">
-             <img class="logo" src="../static/shield.png" alt="UChicago Logo"/>
-           </div>
-           <div class="col-10">
-             <h1>Language Processing Laboratory</h1>
-             <p class="lead">Department of Linguistics, The University of Chicago</p>
-           </div>
-         </div>
-         <div>
-           <p class="mt-4 lead">
-             Thank you for your interest in our study!
-           </p>
-           <p>
-             As a reminder, this study runs best in <b>Chrome</b> or <b>Firefox</b>. If you are not using one of these browers, we recommend switching now to avoid future issues. When you are ready, please proceed by pressing the <strong>space bar</strong>.
-           </p>
-         </div>`,
-  cont_key: [' ']
-}
-
-// Consent form
-var consent = {
-  type: 'consent',
-  requirements: 'You must be at least 18 years old to participate in this study. ',
-  purpose: 'In this research, we are investigating the processes involved in the comprehension of sentences and/or stories. ',
-  procedures: 'In this study, you will be presented with a series of images and descriptions and provide feedback on them, as directed in the experimental instructions. You will also listen to some audio about a set of images. ',
-  time: 'about 30-40 minutes',
-  pay: '$4 USD',
-  name: 'Dr. Ming Xiang',
-  address: '1115 E. 58th St., Rosenwald 205B, Chicago IL, 60637',
-  phone: '(773) 702-8023',
-  email: 'mxiang@uchicago.edu'
-};
-
-// Checks if the subject consented or not
-var check_consent = {
-  conditional_function: function() {
-    var data = jsPsych.data.getLastTrialData();
-    console.log(data.consented);
-    return !data.consented;
-  },
-  timeline: [{
+var pre_experiment_block: [
+  {
     type: 'text',
-    cont_key: [''],
-    text: '<p class="lead">Thank you for considering participation in this study!</p><p>We\'re sorry it wasn\'t for you. You may close this window and return your HIT. There is no penalty for returning our lab\'s HITs.</p>'
-  }]
-};
-
-/* If the subject consents...*/
-var expt_consented = {
+    text: `<div class="header row">
+             <div class="col-2 text-right">
+               <img class="logo" src="../static/shield.png" alt="UChicago Logo"/>
+             </div>
+             <div class="col-10">
+               <h1>Language Processing Laboratory</h1>
+               <p class="lead">Department of Linguistics, The University of Chicago</p>
+             </div>
+           </div>
+           <div>
+             <p class="mt-4 lead">
+               Thank you for your interest in our study!
+             </p>
+             <p>
+               As a reminder, this study runs best in <b>Chrome</b> or <b>Firefox</b>. If you are not using one of these browers, we recommend switching now to avoid future issues. When you are ready, please proceed by pressing the <strong>space bar</strong>.
+             </p>
+           </div>`,
+    cont_key: [' ']
+  },
+  {
+    type: 'consent',
+    requirements: 'You must be at least 18 years old to participate in this study. ',
+    purpose: 'In this research, we are investigating the processes involved in the comprehension of sentences and/or stories. ',
+    procedures: 'In this study, you will be presented with a series of images and descriptions and provide feedback on them, as directed in the experimental instructions. You will also listen to some audio about a set of images. ',
+    time: 'about 30-40 minutes',
+    pay: '$4 USD',
+    name: 'Dr. Ming Xiang',
+    address: '1115 E. 58th St., Rosenwald 205B, Chicago IL, 60637',
+    phone: '(773) 702-8023',
+    email: 'mxiang@uchicago.edu'
+  },
+  {
+    conditional_function: function() {
+      var data = jsPsych.data.getLastTrialData();
+      console.log(data.consented);
+      return !data.consented;
+    },
+    timeline: [{
+      type: 'text',
+      cont_key: [''],
+      text: '<p class="lead">Thank you for considering participation in this study!</p><p>We\'re sorry it wasn\'t for you. You may close this window and return your HIT. There is no penalty for returning our lab\'s HITs.</p>'
+    }];
+  },
+  {
     type: "text",
     text: '<p class="lead mt-4">Thank you for deciding to participate in our study!</p><p>This study has multiple sections. Each section is only a few minutes long. In between sections, you can take short breaks if you need to, but please do not take breaks within a section. When you are ready to begin, please press the <strong>space bar</strong>.</p>',
     choices: [' ']
-};
+  }
+];
 
-/* Generic end-block that works for most sections */
-var end_block = {
+var trial_prefabs = {
+  end_block: {
     type: "text",
     choices: [' '],
     text: function() {
@@ -71,14 +66,73 @@ var end_block = {
     on_finish: function(data){
         saveData(jsPsych.data.getDataAsCSV(), dataRef);
     }
+  },
+  final_block: {
+    type: "text",
+    choices: [''],
+    text: function(){
+        return "<p>You have finished the experiment! Your responses have been saved.</p><p>Your survey code is <b>" + code + "</b>. Please enter this code into your HIT. You may then close this window.</p><p>If you have any questions or concerns, please do not hesitate to contact the lab at <a href='mailto:uchicagolanglab@gmail.com'>uchicagolanglab@gmail.com</a>.";
+    }
+  },
+  audio_test_block_human: {
+    timeline: [{
+        type: "text",
+        choices: ['F', 'J'],
+        text: '<p>You will listen to some verbal statements in this section. Did you turn your speaker on?</p><p>Press <b>F</b> for "yes" and <b>J</b> for "no".</p>',
+        timing_post_trial: 1000
+    }],
+    loop_function: function(data){
+      if(jsPsych.pluginAPI.convertKeyCharacterToKeyCode('j') == data[0].key_press){
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  audio_test_block_synth: {
+    timeline: [{
+        type: "text",
+        choices: ['F', 'J'],
+        text: '<p>We are testing a speech synthesizer that can imitate human voice. In this section you will hear some verbal statements made by this synthesizer. Did you turn your speaker on?</p><p>Press <b>F</b> for "yes" and <b>J</b> for "no".</p>',
+        timing_post_trial: 1000
+    }],
+    loop_function: function(data){
+      if(jsPsych.pluginAPI.convertKeyCharacterToKeyCode('j') == data[0].key_press){
+        return true;
+      } else {
+        return false;
+      }
+    }
+  },
+  end_block_last: {
+      type: "text",
+      choices: [' '],
+      text: function() {
+          return "<p>You have finished this section. You can take a short break now if you want to.</p><p>Please press the space bar when you ready to continue.</p>";
+      },
+      on_finish: function(data){
+          saveData(jsPsych.data.getDataAsCSV(), dataRef);
+          addWorker(workerId, 'adaptation-study');
+      }
+  }
 };
 
-/* Experiment values */
-var trial_distribution = [3,7,10,7,3];
-
-/* The objects we will test on. */
-var objects = [{obj_name: 'candle', property: 'tall', file: 'candle', article: 'a'},{obj_name: 'bar', property: 'bent', file: 'straightrod', article: 'a'},{obj_name: 'pillow', property: 'plain', file: 'pillow', article: 'a'}];
-
-/* Define colorsets */
-var exposure_colors = ['', '_red', '_blue', '_lgreen'];
-var posttest_colors = ['_purple', '_lblue', '_pink'];
+var init_data = {
+  stimuli: [
+    {
+      name: 'candle',
+      adjective: 'tall',
+      article: 'a'
+    },{
+      name: 'bar',
+      adjective: 'bent',
+      article: 'a'
+    },{
+      name: 'pillow',
+      adjective: 'plain',
+      article: 'a'
+    }
+  ],
+  exposure_colors: ['', 'red', 'blue', 'lgreen'],
+  posttest_colors: ['purple', 'lblue', 'pink']
+}
