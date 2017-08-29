ff0b59682fbbb1ccaa4647c7145d611b87292dcd
Narquelion <a.kramer103@gmail.com>
2017-08-29 12:16:54 -0500

    Move pre-experiment_blocks into prefabs and update html for text trials

--- public/static/js/adaptation/data.js
+++ public/static/js/adaptation/data.js
@@ -3,78 +3,77 @@
 
 /* Premade jsPsych trials */
 
-var pre_experiment_block = {
-  type: 'text',
-  timeline: [
-    {
-      type: 'text',
-      text: `<div class="header row">
-               <div class="col-2 text-right">
-                 <img class="logo" src="../static/images/shield.png" alt="UChicago Logo"/>
-               </div>
-               <div class="col-10">
-                 <h1>Language Processing Laboratory</h1>
-                 <p class="lead">Department of Linguistics, The University of Chicago</p>
-               </div>
-             </div>
-             <div>
-               <p class="mt-4 lead">
-                 Thank you for your interest in our study!
-               </p>
-               <p>
-                 As a reminder, this study runs best in <b>Chrome</b> or <b>Firefox</b>. If you are not using one of these browers, we recommend switching now to avoid future issues. When you are ready, please proceed by pressing the <strong>space bar</strong>.
-               </p>
-             </div>`,
-      cont_key: [' ']
-    },
-    {
-      type: 'consent',
-      requirements: 'You must be at least 18 years old to participate in this study. ',
-      purpose: 'In this research, we are investigating the processes involved in the comprehension of sentences and/or stories. ',
-      procedures: 'In this study, you will be presented with a series of images and descriptions and provide feedback on them, as directed in the experimental instructions. You will also listen to some audio about a set of images. ',
-      time: 'about 30-40 minutes',
-      pay: '$4 USD',
-      name: 'Dr. Ming Xiang',
-      address: '1115 E. 58th St., Rosenwald 205B, Chicago IL, 60637',
-      phone: '(773) 702-8023',
-      email: 'mxiang@uchicago.edu'
-    },
-    {
-      conditional_function: function() {
-        var data = jsPsych.data.getLastTrialData();
-        console.log(data.consented);
-        return !data.consented;
-      },
-      timeline: [{
-        type: 'text',
-        cont_key: [''],
-        text: '<p class="lead">Thank you for considering participation in this study!</p><p>We\'re sorry it wasn\'t for you. You may close this window and return your HIT. There is no penalty for returning our lab\'s HITs.</p>'
-      }]
-    },
-    {
-      type: 'demographics'
-    },
-    {
-      type: 'text',
-      text: '<p class="lead mt-4">Thank you for deciding to participate in our study!</p><p>This study has multiple sections. Each section is only a few minutes long. In between sections, you can take short breaks if you need to, but please do not take breaks within a section. When you are ready to begin, please press the <strong>space bar</strong>.</p>',
-      cont_key: [' ']
-    }
-  ]
-};
-
 var prefabs = {
+  pre_experiment_block: {
+    type: 'text',
+    timeline: [
+      {
+        type: 'text',
+        text: `<div class="header row">
+                 <div class="col-2 text-right">
+                   <img class="logo" src="../static/images/shield.png" alt="UChicago Logo"/>
+                 </div>
+                 <div class="col-10">
+                   <h1>Language Processing Laboratory</h1>
+                   <p class="lead">Department of Linguistics, The University of Chicago</p>
+                 </div>
+               </div>
+               <div>
+                 <p class="mt-4 lead">
+                   Thank you for your interest in our study!
+                 </p>
+                 <p>
+                   As a reminder, this study runs best in <b>Chrome</b> or <b>Firefox</b>. If you are not using one of these browers, we recommend switching now to avoid future issues. When you are ready, please proceed by pressing the <strong>space bar</strong>.
+                 </p>
+               </div>`,
+        cont_key: [' ']
+      },
+      {
+        type: 'consent',
+        requirements: 'You must be at least 18 years old to participate in this study. ',
+        purpose: 'In this research, we are investigating the processes involved in the comprehension of sentences and/or stories. ',
+        procedures: 'In this study, you will be presented with a series of images and descriptions and provide feedback on them, as directed in the experimental instructions. You will also listen to some audio about a set of images. ',
+        time: 'about 30-40 minutes',
+        pay: '$4 USD',
+        name: 'Dr. Ming Xiang',
+        address: '1115 E. 58th St., Rosenwald 205B, Chicago IL, 60637',
+        phone: '(773) 702-8023',
+        email: 'mxiang@uchicago.edu'
+      },
+      {
+        conditional_function: function() {
+          var data = jsPsych.data.getLastTrialData();
+          console.log(data.consented);
+          return !data.consented;
+        },
+        timeline: [{
+          type: 'text',
+          cont_key: [''],
+          text: '<p class="lead">Thank you for considering participation in this study!</p><p>We\'re sorry it wasn\'t for you. You may close this window and return your HIT. There is no penalty for returning our lab\'s HITs.</p>'
+        }]
+      },
+      {
+        type: 'demographics'
+      },
+      {
+        type: 'text',
+        text: '<p class="lead mt-4">Thank you for deciding to participate in our study!</p><p>This study has multiple sections. Each section is only a few minutes long. In between sections, you can take short breaks if you need to, but please do not take breaks within a section. When you are ready to begin, please press the <strong>space bar</strong>.</p>',
+        cont_key: [' ']
+      }
+    ]
+  },
   end_block: {
     type: 'text',
     cont_key: [' '],
     text: function() {
-        return `<p>
-                  You have finished this section. You can take a short break now if you want to.
+        return `<p class="lead">
+                  You have finished this section.
                 </p>
                 <p>
-                  Please press the space bar when you ready to continue.
+                  You can take a short break now if you want to. Please press the<strong>space bar</strong>when you ready to continue.
                 </p>`;
     },
-    on_finish: function(data){
+    on_finish: function(){
         saveData(jsPsych.data.dataAsCSV(), dataRef);
     }
   },
@@ -82,7 +81,7 @@ var prefabs = {
     type: 'text',
     cont_key: [''],
     text: function(){
-        return `<p>
+        return `<p class="lead">
                   You have finished the experiment! Your responses have been saved.
                 </p>
                 <p>
@@ -97,14 +96,14 @@ var prefabs = {
     type: 'text',
     cont_key: [' '],
     text: function() {
-        return `<p>
-                  You have finished this section. You can take a short break now if you want to.
+        return `<p class="lead">
+                  You have finished this section.
                </p>
                <p>
-                 Please press the space bar when you ready to continue.
+                 You can take a short break now if you want to. Please press the <strong>space bar</strong> when you ready to continue.
                </p>`;
     },
-    on_finish: function(data){
+    on_finish: function(){
         saveData(jsPsych.data.dataAsCSV(), dataRef);
         addWorker(workerId, 'adaptation-study');
     }
@@ -115,7 +114,7 @@ var prefabs = {
              In this section, you will be shown a series of images.
            </p>
            <p>
-             You will be asked a question about each image. You will respond by pressing buttons on the keyboard.
+             You will be asked a question about each image; follow the on-screen instructions to respond.
              Please answer each question based on your first instinct; there is no right or wrong answer.
           </p>
           <p>
@@ -166,10 +165,10 @@ var audio_test_prefabs = {
 }
 
 var exposure_instructions = {
-  header: '<p>In this section, you will see a sequence of images. ',
+  header: '<p class="lead">In this section, you will see a sequence of images.</p>',
   footer: 'Periodically, you will answer some questions.</p><p>Be sure to remain focused on the center of the screen. ' +
           'Every so often, a \"+\" symbol will be briefly displayed in the center of the screen, and you will be asked a question about it. ' +
-          'Please do your best to answer the question accurately.</p><p>Press the space bar when you are ready to begin.</p>'
+          'Please do your best to answer the question accurately.</p><p>Press the<strong>space bar</strong>when you are ready to begin.</p>'
 }
 
 var params = {
@@ -186,11 +185,6 @@ var params = {
       name: 'pillow',
       adjective: 'plain',
       ambiguous_point: 3
-    },
-    {
-      name: 'flower',
-      adjective: '',
-      ambiguous_point: 4
     }
   ],
   exposure_colors: ['plain', 'red', 'blue', 'lgreen'],
