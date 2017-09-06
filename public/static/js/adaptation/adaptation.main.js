1a97bc3cfe23b0e9cfceaa8301a558eef5c15734
Narquelion <a.kramer103@gmail.com>
2017-09-06 15:27:02 -0500

    Add local load for debugging

--- public/static/js/adaptation/adaptation.main.js
+++ public/static/js/adaptation/adaptation.main.js
@@ -17,10 +17,14 @@ function makeLoadingFun() {
     $('#load-text').html($('#load-text').html() + '.');
 }
 
+var loadMessage = setInterval(function() {
+  makeLoadingFun();
+}, 500);
+
 function loadExperiment() {
   console.log('Worker has not yet completed the experiment.');
 
-  $.getJSON("../static/js/adaptation/params.json", function(json) {
+  $.getJSON("https://language-processing-lab.firebaseapp.com/static/js/adaptation/params.json", function(json) {
 
     var experiment = new AdaptationExperiment(_.extend(json, jsPsych.data.urlVariables()));
     var dataRef = storageRef.child('2-24-2017-run1/' + experiment.getSubjectId() + experiment.getCondition() + experiment.getSubcondition() + experiment.getVoice() + '.csv');
@@ -38,6 +42,64 @@ function loadExperiment() {
 
   }).fail(function(d, textStatus, error) {
     console.error("getJSON failed, status: " + textStatus + ", error: " + error);
+    var params = {
+      stimuli: [
+        {
+          name: 'candle',
+          adjective: 'tall',
+          ambiguous_point: 3
+        },{
+          name: 'bar',
+          adjective: 'bent',
+          ambiguous_point: 3
+        },{
+          name: 'pillow',
+          adjective: 'plain',
+          ambiguous_point: 3
+        }
+      ],
+      calibration: {
+        max_scalepos: 5,
+        distribution: [3,7,10,7,3]
+      },
+      exposure: {
+        instructions: {
+          header: '<p class="lead">In this section, you will be shown a series of images. ',
+          body: {
+            human: 'You will also hear a verbal description of each image. ',
+            synth: 'You will also listen to sentences recorded using a speech synthesizer we are testing. '
+          },
+          footer: 'Periodically, you will answer some questions.</p><p>Be sure to remain focused on the center of the screen. ' +
+                  'Every so often, a \"+\" symbol will be briefly displayed in the center of the screen, and you will be asked a question about it. ' +
+                  'Please do your best to answer the question accurately.</p><p>Press the <strong>space bar</strong> when you are ready to begin.</p>'
+        },
+        colors: ['plain', 'red', 'blue', 'lgreen'],
+        num_audio: 4,
+        reps: 6
+      },
+      posttest: {
+        colors: ['purple', 'lblue', 'pink'],
+        locations: [2, 4, 8, 13, 20, 24]
+      },
+      attention: {
+        colors: ['red', 'blue'],
+        locations: [5, 10, 17, 22]
+      }
+    };
+
+    var experiment = new AdaptationExperiment(_.extend(params, jsPsych.data.urlVariables()));
+    var dataRef = storageRef.child('2-24-2017-run1/' + experiment.getSubjectId() + experiment.getCondition() + experiment.getSubcondition() + experiment.getVoice() + '.csv');
+
+    experiment.createTimeline();
+    experiment.addPropertiesTojsPsych();
+
+    jsPsych.init({
+      timeline: experiment.getTimeline(),
+      show_progress_bar: true,
+      display_element: $('#jspsych-target')
+    });
+
+    clearInterval(loadMessage);
   });
 }
 
@@ -49,17 +111,12 @@ function workerError() {
 }
 
 $( document ).ready(function() {
-
-  var loadMessage = setInterval(function() {
-    makeLoadingFun();
-  }, 500);
-
-  checkWorker(experiment.getSubjectId(), 'adaptation-workers').then(function(snapshot) {
+  checkWorker(jsPsych.data.urlVariables().workerId, 'adaptation-workers').then(function(snapshot) {
     if(snapshot.val() && snapshot.val().complete == 1) {
       workerError();
     }
     else {
       loadExperiment();
     }
-
+  });
 });
